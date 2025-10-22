5f368e39b0aacf6fea34a78070c83a43db83a09e
hmm0411 <23520964@gm.uit.edu.vn>
2025-10-22 14:28:23 +0700

    convert to java

---
+++ app/src/main/java/course/examples/cinepople/MainActivity.java
@@ -0,0 +1,34 @@
+package course.examples.cinepople;
+
+import android.os.Bundle;
+import android.util.Log; // Import the Log class
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.FirebaseApp; // Add a semicolon
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // It's generally recommended to initialize Firebase in an Application class,
+        // but initializing it here will also work.
+        FirebaseApp.initializeApp(this); // Add a semicolon
+        Log.d("FIREBASE", "Firebase initialized successfully!");
+
+        // The following lines seem to be duplicated. You can keep one set.
+        // enableEdgeToEdge(); // This method doesn't exist by default
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

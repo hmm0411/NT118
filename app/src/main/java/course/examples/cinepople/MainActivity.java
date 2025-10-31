d538ec4f98a9ac23151faf4d5f74742aab2ef28a
goshi <goshi.hayashi@gmail.com>
2025-10-31 16:07:07 +0700

    Login Form

--- app/src/main/java/course/examples/cinepople/MainActivity.java
+++ app/src/main/java/course/examples/cinepople/MainActivity.java
@@ -24,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         // The following lines seem to be duplicated. You can keep one set.
         // enableEdgeToEdge(); // This method doesn't exist by default
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;

3ca54810e24e8000c163e71463e65eda51137b93
hmm0411 <23520964@gm.uit.edu.vn>
2025-11-22 20:45:47 +0700

    app pipeline

---
+++ app/src/main/java/course/examples/cinepople/activity/auth/LogoutActivity.java
@@ -0,0 +1,4 @@
+package course.examples.cinepople.activity.auth;
+
+public class LogoutActivity {
+}
---
+++ app/src/main/java/course/examples/cinepople/data/remote/SeatResponse.java
@@ -0,0 +1,4 @@
+package course.examples.cinepople.data.remote;
+
+public class SeatResponse {
+}
